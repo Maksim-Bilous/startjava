af18e446f48f189557e04bb1941d4c8b03d6b30e
Maksim-Bilous <max.biloys@gmail.com>
2025-02-07 11:07:37 +0300

    HW Final

---
+++ lesson 1/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 5;
+        int result = 0;
+        int out = firstNum;
+        char operand = '^';
+
+        if (operand ==  '+' ) {
+            result = firstNum + secondNum;
+        } else if (operand == '-') {
+            result = firstNum - secondNum;
+        } else if (operand == '*') {
+             result = firstNum * secondNum;
+        } else if (operand == '/') {
+             result = firstNum / secondNum;
+        } else if (operand == '%') {
+            result = firstNum % secondNum;
+        }
+        if (operand != '^' ){
+            System.out.println(firstNum + " " + operand + " " + secondNum + " = " + result);
+        } else if (operand == '^') {
+            for (int i = 1; i < secondNum; i++ ) {
+                out = out * firstNum;
+            }
+            System.out.println(firstNum + " " + operand + " " + secondNum + " = " + out);
+        }
+
+    }
+
+}
---
+++ lesson 1/MyFirstGame.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int playerDigit;
+        int pcDigit = 75;
+        int min = 1;
+        int max = 100;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Введите число: ");
+        playerDigit = scanner.nextInt();
+            if (playerDigit == pcDigit){
+                System.out.println("Угадал");
+                break;
+            }else if (playerDigit <= pcDigit) {
+                System.out.println(playerDigit + " меньше того, что загадал компьютер");
+            }else if (playerDigit >= pcDigit) {
+                System.out.println(playerDigit + " больше того, что загадал компьютер");
+            }
+        }
+        if (playerDigit < min) {
+            System.out.println("Число меньше диапазона");
+            break;
+        }else if (playerDigit > max) {
+            System.out.println("Число больше диапазона");
+            break;
+        }
+        }
+    }
